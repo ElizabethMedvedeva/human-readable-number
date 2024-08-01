4ef0fa735ae159146e59bf6ed1f85ffe8ac19b15
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2024-08-01 15:45:45 +0300

    feat: create a function for cnver number to human readable string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const ones = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    const hundreds = [
+        "",
+        "one hundred",
+        "two hundred",
+        "three hundred",
+        "four hundred",
+        "five hundred",
+        "six hundred",
+        "seven hundred",
+        "eight hundred",
+        "nine hundred",
+    ];
+    let result = "";
+    let hundredPart = Math.floor(number / 100);
+    let tenPart = Math.floor((number % 100) / 10);
+    let onePart = number % 10;
+    if (hundredPart > 0) {
+        result += hundreds[hundredPart] + " ";
+    }
+    if (tenPart === 1) {
+        result += teens[(number % 100) - 10];
+    } else {
+        if (tenPart > 1) {
+            result += tens[tenPart] + " ";
+        }
+        if (onePart > 0 || number === 0) {
+            result += ones[onePart];
+        }
+    }
+    return result.trim();
+};
